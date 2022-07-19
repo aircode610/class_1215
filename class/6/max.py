6c22c61b9c4ea6393c57dad114c21c42a2298b25
aircode610 <34022796+aircode610@users.noreply.github.com>
2022-07-19 20:19:32 +0430

    Add files via upload

---
+++ class/6/max.py
@@ -0,0 +1,15 @@
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+c = int(input("enter the third number: ")) 
+mx = 0
+
+if a > mx:
+    mx = a
+if b > mx:
+    mx = b
+if c > mx:
+    mx = c
+
+print(mx)
+
+# print(max(a, b, c))
